7de3f99de731da9d0a3bd61aeb3d1ac5adc6f322
tmhudson <mclean.hudson@kellogg.northwestern.edu>
2023-01-24 06:45:05 +0000

    entering in studio groups

--- kmdb.sql
+++ kmdb.sql
@@ -190,6 +190,160 @@ VALUES (
     "Bruce Wayne"
 );
 
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "Batman Begins",
+    "Michael Caine",
+    "Alfred"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "Batman Begins",
+    "Liam Neeson",
+    "Ra's Al Ghul"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "Batman Begins",
+    "Katie Holmes",
+    "Rachel Dawes"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "Batman Begins",
+    "Gary Oldman",
+    "Commissioner Gordon"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight",
+    "Christian Bale",
+    "Bruce Wayne"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight",
+    "Heath Ledger",
+    "Joker"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight",
+    "Aaron Eckhart",
+    "Harvey Dent"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight",
+    "Michael Caine",
+    "Alfred"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight",
+    "Maggie Gyllenhaal",
+    "Rachel Dawes"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Christian Bale",
+    "Bruce Wayne"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Gary Oldman",
+    "Commissioner Gordon"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Tom Hardy",
+    "Bane"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Joseph Gordon-Levitt",
+    "John Blake"
+);
+
+INSERT INTO castgroups (
+    movie_id,
+    actor_name,
+    character_name
+)
+VALUES (
+    "The Dark Knight Rises",
+    "Anne Hathaway",
+    "Selina Kyle"
+);
+
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
